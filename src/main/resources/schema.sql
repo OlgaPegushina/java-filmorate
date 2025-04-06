185099828ddf517480510132a5ed7a24e25736ab
OlgaPegushina <pegushina.shine@yandex.ru>
2025-04-06 17:27:12 +0300

    ФЗ 13. Добавлены в схему таблицы кроме event

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,6 +5,10 @@ DROP TABLE IF EXISTS film CASCADE;
 DROP TABLE IF EXISTS film_users CASCADE;
 DROP TABLE IF EXISTS genre CASCADE;
 DROP TABLE IF EXISTS film_genre CASCADE;
+DROP TABLE IF EXISTS director CASCADE;
+DROP TABLE IF EXISTS director_film CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS review_users CASCADE;
 
 CREATE TABLE IF NOT EXISTS rating_mpa (
     rating_id INTEGER PRIMARY KEY,
@@ -63,3 +67,42 @@ CREATE TABLE IF NOT EXISTS friendship_status (
     FOREIGN KEY (friend_id) REFERENCES users(user_id)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS director (
+    director_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS director_film (
+    director_id BIGINT,
+    film_id BIGINT,
+    PRIMARY KEY (director_id, film_id),
+    FOREIGN KEY (director_id) REFERENCES director(director_id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (film_id) REFERENCES film(film_id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS review (
+    review_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    film_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    content VARCHAR(255),
+    is_positive BOOLEAN NOT NULL,
+    FOREIGN KEY (film_id) REFERENCES film(film_id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS review_users (
+    review_id BIGINT,
+    user_id BIGINT,
+    rating INTEGER DEFAULT 0,
+    PRIMARY KEY (review_id, user_id),
+    FOREIGN KEY (review_id) REFERENCES review(review_id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
