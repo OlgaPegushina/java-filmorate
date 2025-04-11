3ee944ab634dd46794daef14aa4734947bb18623
Beliaev Dmitry <79296611647@ya.ru>
2025-04-11 18:11:39 +0300

    Функциональность «Лента событий». 3 SP model - feed добавил  @NotNull и @FieldDefaults

--- src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -4,23 +4,31 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import ru.yandex.practicum.filmorate.model.enums.EventOperation;
 import ru.yandex.practicum.filmorate.model.enums.EventType;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Builder
+@FieldDefaults
 public class Feed {
-    private Long timestamp;
+    @NotNull
+    Long timestamp;
 
-    @NotBlank(message = "Значение не может быть пустым")
-    private Long userId;
+    @NotNull(message = "Значение не может быть пустым")
+    Long userId;
 
-    private EventType eventType;
+    @NotNull
+    EventType eventType;
 
-    private Long eventId;
+    @NotNull
+    Long eventId;
 
     @JsonProperty("operation")
-    private EventOperation eventOperation;
+    EventOperation eventOperation;
 
-    private Long entityId;
+    @NotNull
+    Long entityId;
 }
